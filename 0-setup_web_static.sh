8f395b9f695f17f8378dc80aa17c07a943aeb4ab
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-22 18:23:04 -0500

    task 0 attempt 1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,16 +1,61 @@
 #!/usr/bin/env bash
-# Sets up your web servers for the deployment of web_static.
+# Bash script that sets up your web servers for the deployment of web_static.
+# Adds a custom 301 redireection page and custom 404 message
+# Adds header = to X-Served-By $HOSTNAME
+# shellcheck disable=SC2154
 
-if ! [ "$(command -v nginx)" ]
-then
-        sudo apt-get update && apt-get install -y nginx
-fi
-# Create the folder /data/, /data/web_static/ and /data/web_static/releases/ with a single command
-if ! [ -d "/data/web_static/releases/test/" ]; then sudo mkdir -p /data/web_static/releases/test/; fi
-if ! [ -d "/data/web_static/shared/" ]; then sudo mkdir -p /data/web_static/shared/; fi
-echo "Fake HTML File :)" | sudo tee /data/web_static/releases/test/index.html
-# Create a linked symbolic link, if it exists, it must be removed and recreated
+#updates apt repo
+sudo apt-get update -y
+
+# Installs nginx
+sudo apt-get install nginx -y
+
+# Creates the data directory and its children
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Symlink for the test folder
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/ # Give ownership of the /data/ folder to the ubuntu user AND group
-sudo sed -i '55i \\t\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
+# Give ownership to ubuntu user/group
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+
+#unlinks the default file
+unlink /etc/nginx/sites-enabled/default
+
+#creates the config file
+echo "server {
+	listen 80 default_server;
+	index index.html;
+	server_name ferivb.tech;
+	root /var/www/ferivb.tech/html;
+        add_header X-Served-By $HOSTNAME;
+	location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=LFfVDyW6ddg\$request_uri;
+        }
+	error_page 404 /custom_404.html;
+        location = /custom_404.html {
+                root /var/www/ferivb.tech/html;
+                internal;
+        }
+        location /hbnb_static/ {
+                alias /data/web_static/current/;
+        }
+	}
+" > /etc/nginx/conf.d/ferivb.tech.conf
+
+#creates the index file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html;
+
+#creates the index file
+echo "Ceci n'est pas une page" > /var/www/ferivb.tech/html/custom_404.html;
+
+#restarts the server
 sudo service nginx restart
