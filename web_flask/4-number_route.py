dc20f92fec2da9bdeaa57462da30c6309ad38608
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-24 18:47:40 -0500

    task 4 attemtp 1

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+    where '/' displays 'Hello HBNB!'"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello():
+    """ Prints Hello HBNB! on root '/'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_HBNB():
+    """ Prints HBNB! on '/hbnb'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_variable(text):
+    """ displays variable 'text'! on '/c/'"""
+    text = text.replace("_", " ")
+    return "C %s" % escape(text)
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def display_python(text="is cool"):
+    """ something something python something"""
+    text = text.replace("_", " ")
+    return "Python %s" % escape(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_number(n):
+    """ Checks if a variable is an int"""
+    return "%d is a number" % n
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
