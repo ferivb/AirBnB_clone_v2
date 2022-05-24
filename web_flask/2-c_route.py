7bb6b115ddc85b6fe172c46c684e6d7a85acec0b
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-24 14:49:02 -0500

    task 2 attempt 1

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+    where '/' displays 'Hello HBNB!'"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello():
+    """ Prints Hello HBNB! on root '/'"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_HBNB():
+    """ Prints HBNB! on '/hbnb'"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_variable(text):
+    """ displays variable 'text'! on '/c/'"""
+    text = text.replace("_", " ")
+    return "C %s" % escape(text)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
