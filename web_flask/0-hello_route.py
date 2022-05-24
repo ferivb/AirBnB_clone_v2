6a8f42a829b3d7f542d627e0241b90c273405ce7
Felipe Rivas <fe.rivasb@gmail.com>
2022-05-24 14:34:45 -0500

    task 0 attempt 1

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Starts a Flask web application
+    where '/' displays 'Hello HBNB!'"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def display_hello():
+    """ Prints Hello HBNB! on root '/'"""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
